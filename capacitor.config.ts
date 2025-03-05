14a216d2fdeb7206646fbbb61a734e25e6693624
gurjarpravin4 <gurjarpravin4@gmail.com>
2025-03-05 23:16:11 +0530

    initial-setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gp-game.app',
+  appName: 'gp-game',
+  webDir: 'dist'
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { Preferences } from "@capacitor/preferences";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.component.html",
+	styles: [],
 })
 export class AppComponent {
-  title = 'gp-game';
+	title = "gp-game";
+
+	fromCapacitorStorage!: boolean;
+
+	constructor() {
+		Preferences.set({ key: "isLoaded", value: "true" });
+
+		Preferences.get({ key: "isLoaded" }).then((data) => {
+			if (data.value) this.fromCapacitorStorage = data.value == "true";
+		});
+	}
+
+	getValue() {
+		Preferences.get({ key: "isLoaded" }).then((data) => {
+			if (data.value) this.fromCapacitorStorage = data.value == "true";
+		});
+	}
+
+	deleteValue() {
+		Preferences.remove({ key: "isLoaded" });
+	}
 }
