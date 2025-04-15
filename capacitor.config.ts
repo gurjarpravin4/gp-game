db61017132ade7e108aeb03c498d14baad856c1f
gurjarpravin4 <gurjarpravin4@gmail.com>
2025-04-15 20:05:36 +0530

    vikrant-story-apk-generated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.gp-game.app',
-  appName: 'gp-game',
-  webDir: 'dist'
+	appId: "com.karmaquest.app",
+	appName: "gp-game",
+	webDir: "dist/gp-game/browser",
 };
 
 export default config;
